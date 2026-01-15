2505fde41024680550fe49f94f08a53fc02a0e27
akmaalsyh <taufansyah50@gmail.com>
2026-01-15 14:30:47 +0700

    Membuat object DestinasiEntry untuk mendefinisikan rute dan resource judul navigasi halaman entry kader

---
+++ app/src/main/java/com/example/simkader_236/uicontroller/route/DestinasiEntry.kt
@@ -0,0 +1,7 @@
+package com.example.simkader_236.uicontroller.route
+import com.example.simkader_236.R
+
+object DestinasiEntry : DestinasiNavigasi {
+    override val route = "item_entry"
+    override val titleRes = R.string.entry_kader_title
+}
